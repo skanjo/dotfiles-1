7e0d1ca05f25e9469f26d821eadeead08c5814dd
Jacob Kaplan-Moss <jacob@jacobian.org>
2014-12-04 10:53:48 -0800

    Bunches of changes for Yosemite.

--- init.sh
+++ init.sh
@@ -15,7 +15,7 @@ $HOME/.homesick/repos/dotfiles/config-osx.sh
 $HOME/.homesick/repos/dotfiles/setup-dropbox-links.sh
 
 # install homebrew
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # All the homebrew things
 brew bundle $HOME/.homesick/repos/dotfiles/Brewfile
