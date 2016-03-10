dbe7647cc707fb2af292971d4e0302b8fba89a63
Jacob Kaplan-Moss <jacob@jacobian.org>
2016-03-10 07:19:14 -0800

    Updates

--- brew.sh
+++ brew.sh
@@ -9,16 +9,39 @@ set -e
 brew install coreutils sqlite readline openssl
 
 # other stuff
-brew install ack bash-completion gist hub go wget
+brew install ack \
+    bash-completion \
+    gist \
+    hub \
+    go \
+    lastpass-cli \
+    mtr \
+    node \
+    redis \
+    wget
+
+# python, ruby, etc lives in python.sh etc
 
 # cask
-brew install caskroom/cask/brew-cask
+brew tap caskroom/cask
 brew tap caskroom/versions
 brew tap caskroom/fonts
 
 # apps
-brew cask install iterm2 slate yubiswitch sublime-text3
+brew cask install crashplan \
+                  dash \
+                  iterm2-beta \
+                  licecap \
+                  mailplane \
+                  mattr-slate \
+                  nimbus \
+                  nvalt \
+                  postgres \
+                  slack \
+                  sublime-text3
+
 
 # fonts!
-brew cask install font-source-code-pro font-source-sans-pro \
+brew cask install font-source-code-pro \
+                  font-source-sans-pro \
                   font-source-serif-pro
--- python.sh
+++ python.sh
@@ -3,8 +3,11 @@
 brew install python --with-brewed-openssl
 brew install python3 --with-brewed-openssl
 
+pip install --upgrade pip setuptools
+pip3 install --upgrade pip setuptools
+
 pip install virtualenv virtualenvwrapper
 
 curl https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py | python
-
-
+pipsi install lice
+pipsi install pygments
