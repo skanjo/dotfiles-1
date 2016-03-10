7f4ee647869100a8277ce6f4cecf0613917fc937
Jacob Kaplan-Moss <jacob@jacobian.org>
2016-03-10 09:56:32 -0800

    18f-specific stuff

---
+++ 18f.sh
@@ -0,0 +1,10 @@
+# 18f-specific setup
+# Mostly based on https://github.com/18f/laptop
+
+brew tap cloudfoundry/homebrew-tap
+brew install cf-cli
+
+# https://playbook.cio.gov/designstandards/visual-style/#typefaces
+brew cask install font-merriweather font-source-sans-pro
+
+
